5c89e951de559e3bc64f7b3d3313945736ce8133
Andi Gu <andi.gu.ca@gmail.com>
2016-08-31 16:57:02 -0400

    Fixed up the heap.py

--- Resources/Python/ds/heap.py
+++ Resources/Python/ds/heap.py
@@ -1,58 +1,51 @@
-class BinHeap:
+class BinHeap:  # Min heap
     def __init__(self):
-        self.heapList = [0] # header (not represented)
+        self.heapList = [0]  # never accessed (0th index just to make accessing simpler)
         self.currentSize = 0
 
-    def insert(self, value):
+    def push(self, value):
         self.heapList.append(value)
         self.currentSize += 1
+        self.shift_up(self.currentSize)
 
-        # maintain heap property
-        i = self.currentSize
-        while i // 2 > 0:
-            if self.heapList[i] < self.heapList[i // 2]: # swap parent and child
-                temp = self.heapList[i // 2]
-                self.heapList[i // 2] = self.heapList[i]
-                self.heapList[i] = temp
-            i //= 2
-
-    def min_child(self, i):
-        if i * 2 + 1 > self.currentSize:
-            return i * 2
-        else:
-            if self.heapList[i * 2] < self.heapList[i * 2 + 1]:
-                return i * 2
-            else:
-                return i * 2 + 1
-
-    def delete_min(self):
-        retval = self.heapList[1]
+    def pop(self):
+        smallest = self.heapList[1]
         self.heapList[1] = self.heapList[self.currentSize]
         self.currentSize -= 1
         self.heapList.pop()
+        self.shift_down(1)
+        return smallest
 
-        # maintain heap property
-        i = 1
-        while (i * 2) <= self.currentSize:
-            mc = self.min_child(i)
-            if self.heapList[i] > self.heapList[mc]:
-                tmp = self.heapList[i]
-                self.heapList[i] = self.heapList[mc]
-                self.heapList[mc] = tmp
-            i = mc
+    def min_child(self, index):
+        if index * 2 + 1 > self.currentSize:
+            return index * 2
+        else:
+            if self.heapList[index * 2] < self.heapList[index * 2 + 1]:
+                return index * 2
+            else:
+                return index * 2 + 1
 
-        return retval
-
-    def build_heap(self, alist):
-        i = len(alist) // 2
-        self.currentSize = len(alist)
-        self.heapList = [0] + alist[:]
+    def build_heap(self, lst):
+        i = len(lst) // 2
+        self.currentSize = len(lst)
+        self.heapList = [0] + lst[:]
         while i > 0:
-            while (i * 2) <= self.currentSize:
-                mc = self.min_child(i)
-                if self.heapList[i] > self.heapList[mc]:
-                    tmp = self.heapList[i]
-                    self.heapList[i] = self.heapList[mc]
-                    self.heapList[mc] = tmp
-                i = mc
+            self.shift_down(i)
             i -= 1
+
+    def shift_up(self, index):
+        while index // 2 > 0:
+            if self.heapList[index] < self.heapList[index // 2]:
+                temp = self.heapList[index // 2]
+                self.heapList[index // 2] = self.heapList[index]
+                self.heapList[index] = temp
+            index //= 2
+
+    def shift_down(self, index):
+        while (index * 2) <= self.currentSize:
+            mc = self.min_child(index)
+            if self.heapList[index] > self.heapList[mc]:
+                tmp = self.heapList[index]
+                self.heapList[index] = self.heapList[mc]
+                self.heapList[mc] = tmp
+            index = mc
--- Resources/Python/ds/priority_queue.py
+++ Resources/Python/ds/priority_queue.py
@@ -1,2 +1,11 @@
+from ds.heap import BinHeap
+
+
 class PriorityQueue:
-    
+    def __init__(self):
+        self.heap = BinHeap()
+
+    def insert(self, key, item):
+        self.heap.push((key, item))
+
+    def
