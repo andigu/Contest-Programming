f78e5dc7c3b31a9668749e7f080a50f043abf4e4
Andi Gu <andi.gu.ca@gmail.com>
2016-10-01 15:30:42 -0400

    Started working on NOI problem

--- DMOJ/graph/cco13p3.py
+++ DMOJ/graph/cco13p3.py
@@ -1,5 +1,6 @@
 import sys
 
+
 def dfs(current, depth, array):
     array[current] = depth
     depth += 1
@@ -17,14 +18,8 @@ for i in range(n - 1):
     graph[b].add(a)
 depthA = [-1] * n
 dfs(0, 0, depthA)
-maxA = max(depthA)
-furthest = depthA.index(maxA)
+furthest = depthA.index(max(depthA))
 depthB = [-1] * n
 dfs(furthest, 0, depthB)
-maxB = max(depthB)
-count = 0
-for i in range(n):
-    if depthA[i] == maxA and depthB[i] == maxB:
-        count += 1
-num = (depthB.count(maxB)) * (depthA.count(max(depthA)) - count)
-print(str(maxB + 1) + " " + str(num))
+largest = max(depthB)
+print(largest + 1, depthB.count(largest) * depthA.count(max(depthA)))
---
+++ WCIPEG/ds/noi04p1.py
@@ -0,0 +1,50 @@
+import sys
+from bisect import insort
+
+
+def update(x):
+    if x != 0:
+        global size, arr, left
+        for i in xrange(size - 1, -1, -1):
+            arr[i] += x
+            if arr[i] < min:
+                arr = arr[i + 1:]
+                size = size - i - 1
+                left += i + 1
+                return
+
+
+data = (i.split() for i in sys.stdin.read().strip().split("\n"))
+n, min = [int(i) for i in data.next()]
+arr = []
+size = 0
+left = 0
+to_add = 0
+for letter, number in data:
+    number = int(number)
+    if letter == "I" and number >= min:
+        update(to_add)
+        to_add = 0
+        insort(arr, number, 0, size)
+        size += 1
+    elif letter == "A":
+        if to_add < 0:
+            update(to_add)
+            to_add = 0
+        to_add += number
+    elif letter == "S":
+        to_add -= number
+    elif letter == "F":
+        if to_add < 0:
+            update(to_add)
+            to_add = 0
+            if number <= size:
+                print(arr[-number])
+            else:
+                print(-1)
+        else:
+            if number <= size:
+                print(arr[-number] + to_add)
+            else:
+                print(-1)
+print(left)
