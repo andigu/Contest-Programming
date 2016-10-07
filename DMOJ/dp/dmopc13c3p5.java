f809a8ef05ba2c50118cab0adadcb77ff4cac96d
Andi Gu <Andi Gu>
2016-10-06 23:31:43 -0400

    Finished dmopc problem

---
+++ DMOJ/dp/dmopc13c3p5.java
@@ -0,0 +1,40 @@
+package dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * @author Andi Gu
+ */
+public class dmopc13c3p5 {
+    public static void main(String[] tokens) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        tokens = reader.readLine().split(" ");
+        int timeLimit = Integer.parseInt(tokens[0]);
+        int foodLimit = Integer.parseInt(tokens[1]);
+        int n = Integer.parseInt(tokens[2]);
+        int[][] r = new int[n][3];
+        for (int i=0; i < n; i++) {
+            tokens = reader.readLine().split(" ");
+            r[i][0] = Integer.parseInt(tokens[0]);
+            r[i][1] = Integer.parseInt(tokens[1]);
+            r[i][2] = Integer.parseInt(tokens[2]);
+        }
+        int[][][] dp = new int[n + 1][foodLimit + 1][timeLimit + 1];
+        for (int i=1; i <= n; i++) {
+            for (int f=1; f <= foodLimit; f++) {
+                for (int t=1; t <= timeLimit; t++) {
+                    if (r[i - 1][1] > t || r[i - 1][2] > f) {
+                        dp[i][f][t] = dp[i - 1][f][t];
+                    }
+                    else {
+                        dp[i][f][t] = Math.max(dp[i - 1][f][t],
+                                dp[i - 1][f - r[i - 1][2]][t - r[i - 1][1]] + r[i - 1][0]);
+                    }
+                }
+            }
+        }
+        System.out.println(dp[n][foodLimit][timeLimit]);
+    }
+}
--- DMOJ/dp/dmopc13c3p5.py
+++ DMOJ/dp/dmopc13c3p5.py
@@ -1,26 +1,6 @@
-time_limit, food_limit, n = [int(i) for i in input().split()]
-restaurants = [[int(i) for i in input().split()] for _ in range(n)]
-dp = [[0] * (time_limit + 1) for i in range(n + 1)]
-food = [[0] * (time_limit + 1) for i in range(n + 1)]
-for i in range(1, n + 1):
-    for t in range(time_limit + 1):
-        if restaurants[i - 1][1] > t:
-            dp[i][t] = dp[i - 1][t]
-            food[i][t] = food[i - 1][t]
-        else:
-            if food[i - 1][t - restaurants[i - 1][1]] + restaurants[i - 1][2] > food_limit:
-                dp[i][t] = dp[i - 1][t]
-                food[i][t] = food[i - 1][t]
-                print(i, t)
-            else:
-                if dp[i - 1][t - restaurants[i - 1][1]] + restaurants[i - 1][0] > dp[i - 1][t]:
-                    dp[i][t] = dp[i - 1][t - restaurants[i - 1][1]] + restaurants[i - 1][0]
-                    food[i][t] = food[i - 1][t - restaurants[i - 1][1]] + restaurants[i - 1][2]
-                else:
-                    dp[i][t] = dp[i - 1][t]
-                    food[i][t] = food[i - 1][t]
-for i in dp:
-    print(i)
-print()
-for i in food:
-    print(i)
+m, u, r = [int(i) for i in input().split()]
+dp = [[0] * (u + 1) for i in range(m + 1)]
+for _ in range(r):
+    v, t, f = [int(i) for i in input().split()]
+    dp = [[max(dp[i][j], dp[i - t][j - f] + v if i >= t and j >= f else 0) for j in range(u + 1)] for i in range(m + 1)]
+print(dp[-1][-1])
